68c6c48dfda450f066d522414d0d74379f57c4c2
Washington Sampaio <washingtonsampaio.23@gmail.com>
2019-09-24 16:59:49 -0300

    up

--- backup pelo cmd.bat
+++ backup pelo cmd.bat
@@ -1,8 +1,11 @@
-ECHO Fazendo bkp do banco bd_locadora_w e salvando na pasta na area de trabalho 
-c:
-cd "C:\Users\washi\Desktop"
-md backup
-c:
-cd "C:\Program Files\MySQL\MySQL Server 8.0\bin" 
-mysqldump -u root -p bd_locadora_w > c:\Users\washi\Desktop\backup\backupLocadora.sql 
+@echo off
+REM Fazendo bkp do banco bd_locadora_w e salvando na pasta na area de trabalho 
 
+c:
+cd %USERPROFILE%\Desktop
+md backup
+cd "C:\Program Files\MySQL\MySQL Server 8.0\bin" 
+mysqldump -u root -pbcd127 db_godinner > %USERPROFILE%\desktop\TCC-API\Banco_de_dados\backupLocadora.sql 
+@echo on
+pause
+REM -@- By. Marlon -@-
