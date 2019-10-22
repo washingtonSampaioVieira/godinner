64c0c46111037a478f92df785e06f248d7d43682
MARINA SANTANA VAZ <18175241@CFP127ALUNO.LOCAL>
2019-10-22 13:24:13 -0300

    Template daquele restaurante, jé está pronto

--- godinner-api/app/src/main/java/godinner/app/repository/TemplateRestauranteRepository.java
+++ godinner-api/app/src/main/java/godinner/app/repository/TemplateRestauranteRepository.java
@@ -4,10 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import godinner.app.helper.Template;
+import godinner.app.model.Produto;
 import godinner.app.model.TemplateRestaurante;
 
 public interface TemplateRestauranteRepository extends JpaRepository<TemplateRestaurante, Long> {
 	
+	@Query("SELECT t FROM TemplateRestaurante t WHERE t.restaurante.id = ?1")
+	TemplateRestaurante getTemplate(int idRestaurante);
 	
 
 }
--- godinner-api/app/src/main/java/godinner/app/resource/TemplateRestauranteResource.java
+++ godinner-api/app/src/main/java/godinner/app/resource/TemplateRestauranteResource.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,6 +41,15 @@ public class TemplateRestauranteResource {
 	public TemplateRestaurante setTemplateRestaurante(@Validated @RequestBody TemplateRestaurante tr) {
 	
 		TemplateRestaurante templateRestaurante = templateRestauranteRepository.save(tr);
+		
+		return templateRestaurante;
+	}
+	
+	@GetMapping("/restaurantes/{idRestaurante}")
+	public TemplateRestaurante getTemplate(@PathVariable int idRestaurante) {
+		TemplateRestaurante templateRestaurante = templateRestauranteRepository.getTemplate(idRestaurante);
+		
+		templateRestaurante.getRestaurante().setSenha(null);
 		
 		return templateRestaurante;
 	}
